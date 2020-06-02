adbb61c08b933a0edfff629430c2354dcbce7ed2
LipeDuarte <65302482+LipeDuarte@users.noreply.github.com>
2020-06-01 23:06:45 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,80 @@
+// Debounce do Lodash
+debounce = function(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+
+(function()
+         {
+	var $target = $('.legenda'),
+			animationClass = 'anime-start';
+
+    function animeScroll() 
+    {
+		var documentTop = $(document).scrollTop();
+
+        $target.each(function()
+        {
+			var itemTop = $(this).offset().top;
+            if (documentTop > itemTop - 600) 
+            {
+				$(this).addClass(animationClass);
+            } else 
+            {
+				$(this).removeClass(animationClass);
+			}
+		});
+    }
+    
+
+
+	animeScroll();
+
+	$(document).scroll(debounce(function(){
+		animeScroll();
+	}, 200));
+})();
+
+
+
+(function()
+         {
+	var $target = $('.linha'),
+			animationClass = 'linha_anime';
+
+    function animeLinha() 
+    {
+		var documentTop = $(document).scrollTop();
+
+        $target.each(function()
+        {
+			var itemTop = $(this).offset().top;
+            if (documentTop > itemTop - 400) 
+            {
+				$(this).addClass(animationClass);
+            } else 
+            {
+				$(this).removeClass(animationClass);
+			}
+		});
+    }
+    
+
+
+	animeLinha();
+
+	$(document).scroll(debounce(function(){
+		animeLinha();
+	}, 200));
+})();
